16c97f86ecc719adf17cf0d7e984b8f2d539d639
ChandraReddy74 <chandrasekhar.scm07@gmail.com>
2018-08-25 05:42:20 +0000

    2nd changes

--- firstjava.java
+++ firstjava.java
@@ -2,6 +2,7 @@ public class HelloWorld
 {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
-                System.out.println("chandra ");
+                System.out.println("chandra i");
+                System.out.println("chandra reddy");
 	}
 }
